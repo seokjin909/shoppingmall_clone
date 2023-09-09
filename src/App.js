166e56cdbd3927a226c65725c3e356eda462b2b9
jin <thseokjin@naver.com>
2023-09-09 17:47:00 +0900

    라우터 설정 및 폴더 구조화

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+const queryClient = new QueryClient();
+function App() {
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Navbar />
+        <Outlet />
+      </QueryClientProvider>
+    </>
+  );
+}
+
+export default App;
